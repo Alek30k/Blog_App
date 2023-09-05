eb038bce67e3f35b5f16e9ec0f15c83e07dff607
Alek30k <alemank23k@gmail.com>
2023-09-05 17:34:55 -0300

    last commit

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import styles from "./themeToggle.module.css";
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+  return (
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="" width={14} height={14} />
+      <div
+        className={styles.ball}
+        // style={
+        //   theme === "dark"
+        //     ? { left: 1, background: "#0f172a" }
+        //     : { right: 1, background: "white" }
+        // }
+      ></div>
+      <Image src="/sun.png" alt="" width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
