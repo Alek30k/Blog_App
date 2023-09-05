0ae30c8f0391c4ba871ded772b0dcc1f030165ac
Alek30k <alemank23k@gmail.com>
2023-09-05 16:05:34 -0300

    last commit

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./menu.module.css";
+import styles from "./menu.module.css";
+
+const Menu = () => {
+  return <div className={styles.container}>Menu</div>;
+};
+
+export default Menu;
