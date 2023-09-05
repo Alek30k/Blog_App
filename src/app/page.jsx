914bb9bb7441940ed5cc3c3cc43ad3487211ab6f
Alek30k <alemank23k@gmail.com>
2023-09-04 23:49:31 -0300

    last commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,54 +1,13 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return (
     <div>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic neque
-        rerum accusantium assumenda repellendus odit obcaecati qui quidem?
-        Cumque voluptate odio non voluptates! Ipsum sit aliquid numquam itaque
-        error tempore!
-      </h1>
-      <Link href="/">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi ab, unde
-        aperiam aspernatur non maiores nulla fugiat exercitationem quae quis
-        provident explicabo beatae sit, voluptatem, perferendis rerum quam aut
-        minus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-        ab, unde aperiam aspernatur non maiores nulla fugiat exercitationem quae
-        quis provident explicabo beatae sit, voluptatem, perferendis rerum quam
-        aut minus! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Modi ab, unde aperiam aspernatur non maiores nulla fugiat exercitationem
-        quae quis provident explicabo beatae sit, voluptatem, perferendis rerum
-        quam aut minus! Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Modi ab, unde aperiam aspernatur non maiores nulla fugiat
-        exercitationem quae quis provident explicabo beatae sit, voluptatem,
-        perferendis rerum quam aut minus! Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Modi ab, unde aperiam aspernatur non
-        maiores nulla fugiat exercitationem quae quis provident explicabo beatae
-        sit, voluptatem, perferendis rerum quam aut minus! Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit. Modi ab, unde aperiam aspernatur non
-        maiores nulla fugiat exercitationem quae quis provident explicabo beatae
-        sit, voluptatem, perferendis rerum quam aut minus! Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit. Modi ab, unde aperiam aspernatur non
-        maiores nulla fugiat exercitationem quae quis provident explicabo beatae
-        sit, voluptatem, perferendis rerum quam aut minus! Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit. Modi ab, unde aperiam aspernatur non
-        maiores nulla fugiat exercitationem quae quis provident explicabo beatae
-        sit, voluptatem, perferendis rerum quam aut minus! Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit. Modi ab, unde aperiam aspernatur non
-        maiores nulla fugiat exercitationem quae quis provident explicabo beatae
-        sit, voluptatem, perferendis rerum quam aut minus! Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit. Modi ab, unde aperiam aspernatur non
-        maiores nulla fugiat exercitationem quae quis provident explicabo beatae
-        sit, voluptatem, perferendis rerum quam aut minus! Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit. Modi ab, unde aperiam aspernatur non
-        maiores nulla fugiat exercitationem quae quis provident explicabo beatae
-        sit, voluptatem, perferendis rerum quam aut minus! Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit. Modi ab, unde aperiam aspernatur non
-        maiores nulla fugiat exercitationem quae quis provident explicabo beatae
-        sit, voluptatem, perferendis rerum quam aut minus!
-      </Link>
+      <Navbar />
+      <Footer />
     </div>
   );
 }
