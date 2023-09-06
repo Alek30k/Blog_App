158eb6a7d1f134e7c8c223293c65d915d4dfb14a
Alek30k <alemank23k@gmail.com>
2023-09-06 17:37:17 -0300

    last commit

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./authLinks.module.css";
+import { useState } from "react";
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
   const status = "notauthaenticated";
 
   return (
@@ -14,6 +18,12 @@ const AuthLinks = () => {
           <span className={styles.link}>Logout</span>
         </>
       )}
+
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
     </>
   );
 };
