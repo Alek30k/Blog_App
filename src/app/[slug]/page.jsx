45f2e6c6882aae56aa0fc8088e544bc52296559d
Alek30k <alemank23k@gmail.com>
2023-09-09 22:56:39 -0300

    last commit

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,4 +1,4 @@
-// import Menu from "@/components/Menu/Menu";
+import Menu from "@/components/menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
 // import Comments from "@/components/comments/Comments";
@@ -24,29 +24,28 @@ const SinglePage = async ({ params }) => {
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          {/* <h1 className={styles.title}>{data?.title}</h1> */}
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+            assumenda.
+          </h1>
           <div className={styles.user}>
-            {/* {data?.user?.image && (
-              <div className={styles.userImageContainer}>
-                <Image
-                  src={data.user.image}
-                  alt=""
-                  fill
-                  className={styles.avatar}
-                />
-              </div>
-            )} */}
+            {/* {data?.user?.image && ( */}
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
+            </div>
+            {/* )} */}
             <div className={styles.userTextContainer}>
               {/* <span className={styles.username}>{data?.user.name}</span> */}
+              <span className={styles.username}>John Doe</span>
               <span className={styles.date}>01.01.2024</span>
             </div>
           </div>
         </div>
-        {/* {data?.img && (
-          <div className={styles.imageContainer}>
-            <Image src={data.img} alt="" fill className={styles.image} />
-          </div>
-        )} */}
+        {/* {data?.img && ( */}
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        {/* )} */}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
@@ -58,7 +57,7 @@ const SinglePage = async ({ params }) => {
             {/* <Comments postSlug={slug} /> */}
           </div>
         </div>
-        {/* <Menu /> */}
+        <Menu />
       </div>
     </div>
   );
