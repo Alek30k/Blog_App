1c5a7ec9c112d9da5243de1dd7bc373c4107a905
Alek30k <alemank23k@gmail.com>
2023-09-09 22:19:21 -0300

    last commit

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,32 +1,32 @@
-import Menu from "@/components/Menu/Menu";
+// import Menu from "@/components/Menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
-import Comments from "@/components/comments/Comments";
+// import Comments from "@/components/comments/Comments";
 
-const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
-    cache: "no-store",
-  });
+// const getData = async (slug) => {
+//   const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    throw new Error("Failed");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 const SinglePage = async ({ params }) => {
   const { slug } = params;
 
-  const data = await getData(slug);
+  //   const data = await getData(slug);
 
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>{data?.title}</h1>
+          {/* <h1 className={styles.title}>{data?.title}</h1> */}
           <div className={styles.user}>
-            {data?.user?.image && (
+            {/* {data?.user?.image && (
               <div className={styles.userImageContainer}>
                 <Image
                   src={data.user.image}
@@ -35,30 +35,30 @@ const SinglePage = async ({ params }) => {
                   className={styles.avatar}
                 />
               </div>
-            )}
+            )} */}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>{data?.user.name}</span>
+              {/* <span className={styles.username}>{data?.user.name}</span> */}
               <span className={styles.date}>01.01.2024</span>
             </div>
           </div>
         </div>
-        {data?.img && (
+        {/* {data?.img && (
           <div className={styles.imageContainer}>
             <Image src={data.img} alt="" fill className={styles.image} />
           </div>
-        )}
+        )} */}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div
+          {/* <div
             className={styles.description}
             dangerouslySetInnerHTML={{ __html: data?.desc }}
-          />
+          /> */}
           <div className={styles.comment}>
-            <Comments postSlug={slug} />
+            {/* <Comments postSlug={slug} /> */}
           </div>
         </div>
-        <Menu />
+        {/* <Menu /> */}
       </div>
     </div>
   );
