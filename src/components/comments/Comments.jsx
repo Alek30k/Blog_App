6540fa9de10b9b92c0b0f09d82ba21284db63f03
Alek30k <alemank23k@gmail.com>
2023-09-11 13:38:42 -0300

    last commit

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,70 +1,86 @@
-.container {
-    margin-top: 50px;
+"use client";
+
+import Link from "next/link";
+import styles from "./comments.module.css";
+import Image from "next/image";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(data.message);
+    throw error;
   }
-  
-  .title {
-    color: var(--softTextColor);
-    margin-bottom: 30px;
-  }
-  
-  .write {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: 30px;
-  }
-  
-  .input {
-    padding: 20px;
-    width: 100%;
-  }
-  
-  .button {
-    padding: 16px 20px;
-    background-color: teal;
-    color: white;
-    font-weight: bold;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  
-  .comments{
-    margin-top: 50px;
-  }
-  
-  .comment{
-    margin-bottom: 50px;
-  }
-  
-  .user {
-    display: flex;
-    align-items: center;
-    gap: 20px;
-    margin-bottom: 20px;
-  }
-  
-  .image {
-    border-radius: 50%;
-    object-fit: cover;
-  }
-  
-  .userInfo {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-    color: var(--softTextColor);
-  }
-  
-  .username {
-    font-weight: 500;
-  }
-  
-  .date{
-    font-size: 14px;
-  }
-  
-  .desc{
-    font-size: 18px;
-    font-weight: 300;
-  }
+
+  return data;
+};
+
+const Comments = ({ postSlug }) => {
+  const { status } = useSession();
+
+  const { data, mutate, isLoading } = useSWR(
+    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    fetcher
+  );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+    mutate();
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === "authenticated" ? (
+        <div className={styles.write}>
+          <textarea
+            placeholder="write a comment..."
+            className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
+        </div>
+      ) : (
+        <Link href="/login">Login to write a comment</Link>
+      )}
+      <div className={styles.comments}>
+        {isLoading
+          ? "loading"
+          : data?.map((item) => (
+              <div className={styles.comment} key={item._id}>
+                <div className={styles.user}>
+                  {item?.user?.image && (
+                    <Image
+                      src={item.user.image}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                    />
+                  )}
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}>{item.user.name}</span>
+                    <span className={styles.date}>{item.createdAt}</span>
+                  </div>
+                </div>
+                <p className={styles.desc}>{item.desc}</p>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
