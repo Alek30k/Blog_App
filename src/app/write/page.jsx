48294fd590461316032911eff9e92dafec1f64aa
Alek30k <alemank23k@gmail.com>
2023-09-16 15:28:41 -0300

    last commit

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import styles from "./writePage.module.css";
 import { useEffect, useState } from "react";
-// import "react-quill/dist/quill.bubble.css";
+import "react-quill/dist/quill.bubble.css";
 import { useRouter } from "next/navigation";
 // import { useSession } from "next-auth/react";
 // import {
@@ -13,7 +13,7 @@ import { useRouter } from "next/navigation";
 //   getDownloadURL,
 // } from "firebase/storage";
 // import { app } from "@/utils/firebase";
-// import ReactQuill from "react-quill";
+import ReactQuill from "react-quill";
 
 const WritePage = () => {
   //   const { status } = useSession();
@@ -139,13 +139,13 @@ const WritePage = () => {
             </button>
           </div>
         )}
-        {/* <ReactQuill
+        <ReactQuill
           className={styles.textArea}
           theme="bubble"
           value={value}
           onChange={setValue}
           placeholder="Tell your story..."
-        /> */}
+        />
       </div>
       <button className={styles.publish} onClick={handleSubmit}>
         Publish
