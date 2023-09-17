fa350c1c2ec98c33b6971f59a54fc8396b104211
Alek30k <alemank23k@gmail.com>
2023-09-16 23:01:21 -0300

    last commit

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,12 @@
 "use client";
-// import { signIn, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import styles from "./loginPage.module.css";
 import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
-  //   const { status } = useSession();
+  const { data, status } = useSession();
+
+  console.log(data, status);
 
   const router = useRouter();
 
@@ -19,9 +21,9 @@ const LoginPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        {/* <div className={styles.socialButton} onClick={() => signIn("google")}>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
           Sign in with Google
-        </div> */}
+        </div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
