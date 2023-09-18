aa84ef2a3f04fad824ae444d72a8d790ee8e1533
Alek30k <alemank23k@gmail.com>
2023-09-18 14:29:21 -0300

    last commit

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 const LoginPage = () => {
   const { data, status } = useSession();
 
-  console.log(data, status);
+  // console.log(data, status);
 
   const router = useRouter();
 
