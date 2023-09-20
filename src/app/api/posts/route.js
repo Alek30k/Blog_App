21ef1d842c927d21326fe825fb99f3946501132b
Alek30k <alemank23k@gmail.com>
2023-09-19 21:37:38 -0300

    last commit

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,19 @@
+import { getAuthSession } from "@/utils/auth";
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+export const GET = async (req) => {
+  const { searchParams } = new URL(req.url);
+
+  const page = searchParams.get("page");
+  const cat = searchParams.get("cat");
+
+  const POST_PER_PAGE = 2;
+
+  const query = {
+    take: POST_PER_PAGE,
+    skip: POST_PER_PAGE * (page - 1),
+    where: {
+      ...(cat && { catSlug: cat }),
+    },
+  };
