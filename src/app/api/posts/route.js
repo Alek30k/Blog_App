d99edb8b8873eb0e2ea3749af2d72156672dc71b
Alek30k <alemank23k@gmail.com>
2023-09-19 21:38:15 -0300

    last commit

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -17,3 +17,17 @@ export const GET = async (req) => {
       ...(cat && { catSlug: cat }),
     },
   };
+
+  try {
+    const [posts, count] = await prisma.$transaction([
+      prisma.post.findMany(query),
+      prisma.post.count({ where: query.where }),
+    ]);
+    return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
